ce5d0c7e1a9cb2df95c0a5d9256a331990e1678b
leon <LeonKogo20@gmail.com>
2025-05-11 21:33:11 +0300

    add untracked files

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    
+    fibonacci = [0]
+    if length == 1:
+        print(fibonacci)
+        return
+    
+    fibonacci.append(1)
+    for i in range(2, length):
+        fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
+    
+    print(fibonacci)
